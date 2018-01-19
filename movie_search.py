94121a023224e9eaa4261780f1b40811d3343247
Mathe13 <matheuslimaaa13@gmail.com>
2018-01-19 10:15:55 -0200

    finalizado

--- movie_search.py
+++ movie_search.py
@@ -2,19 +2,25 @@ import sys
 import requests
 import json
 
-def get_data_movie(movie_name,parametro):
+def get_data_movie(movie_name,parametro,pagina=1):
     try:
-        req=requests.get(url='http://www.omdbapi.com/?i=tt3896198&apikey=b34f7b7b&'+parametro+'='+movie_name+'&type=movie')
+        req=requests.get(url='http://www.omdbapi.com/?i=tt3896198&apikey=b34f7b7b&'+parametro+'='+movie_name+'&type=movie&page='+pagina)
+        #print('http://www.omdbapi.com/?i=tt3896198&apikey=b34f7b7b&'+parametro+'='+movie_name+'&type=movie&page='+pagina)
         resposta=json.loads(req.text)
         if resposta['Response']=='False':
             print('filme não encontrado')
             sys.exit()
         else:
-            print('teste')
             return resposta
     except Exception as e:
         print('erro:',e)
 
+def mostra_pagina(dados,movie_name):
+    #resposta=get_data_movie(str(sys.argv[2:]),'s'+str(pagina),str(pagina))
+    #print (type(dados))
+    for i in dados.get('Search'):
+        #print('teste')
+        print_movie_data(i)
 
 
 def print_movie_data(movie_data):
@@ -42,18 +48,24 @@ def main():
         print("use: movie_search -op movie_name")
     else:
         if(op=='-s'):
-            print('todo')
-            resposta=get_data_movie(str(sys.argv[2:]),'s')
+            resposta=get_data_movie(str(sys.argv[2:]),'s','1')
+            numberOfPages=int(resposta.get('totalResults'))//9
             print('existem',resposta.get('totalResults'),'resultado para sua busca')
-            ver=input('deseja ver os 10 primeiros?(y/n)')
+            ver=input('deseja ver os resultados?(y/n)')
             if(ver=='y' or ver=='Y'):
-                for i in resposta.get('Search'):
-                    #print('teste')
-                    print_movie_data(i)
+                for i in range(0,numberOfPages):
+                    print (i)
+                    pagina=i+1
+                    resposta=get_data_movie(str(sys.argv[2:]),'s',str(pagina))
+                    mostra_pagina(resposta,pagina)
+                    continua=input('continue?(y,n)')
+                    if(continua=='n' or continua=='n'):
+                        sys.exit()
             else:
-                print('todo')
+                print('ok,saindo...')
+                sys.exit()
         elif(op=='-t'):
-            resposta=get_data_movie(str(sys.argv[2:]),'t')
+            resposta=get_data_movie(str(sys.argv[2:]),'t','1')
             print_movie_data(resposta)
         else:
             print('opção desconhecida:use -t ou -s')
